bd70849160b2ca8245f28738e9136c149ad77f0f
AnkLaV001 <aanklav@gmail.com>
2021-11-30 08:53:39 +0300

    Lesson3_1

---
+++ WebUI_Java/Lesson2/example_andre/src/main/java/Lesson3/SeleniumStart.java
@@ -0,0 +1,9 @@
+package Lesson3;
+
+public class SeleniumStart {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+    }
+}
