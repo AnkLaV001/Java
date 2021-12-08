e49742f120e422cf2c0625faeb8a321b355b02c4
AnkLaV001 <aanklav@gmail.com>
2021-12-08 08:54:46 +0300

    Lesson4

---
+++ WebUI_Java/Lesson2/example_andre/src/main/java/Lesson4/Triangle.java
@@ -0,0 +1,4 @@
+package Lesson4;
+
+public class Triangle {
+}
---
+++ WebUI_Java/Lesson2/example_andre/src/test/java/Lesson4/TriangleAreaTest.java
@@ -0,0 +1,14 @@
+package Lesson4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TriangleAreaTest {
+    @Test
+    void calculateSquerWentOneNumberIsNull(){
+        float result = TriangleArea.formulaTriangleArea(7, 10, 0);
+        assertTrue
+    }
+
+}
