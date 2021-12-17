312e9398cb025e8443ddd77e6e86394ec569534f
AnkLaV001 <aanklav@gmail.com>
2021-12-17 08:51:48 +0300

    Lesson6

---
+++ WebUI_Java/Lesson2/example_andre/src/main/java/Lesson6/BaseView.java
@@ -0,0 +1,21 @@
+package Lesson6;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseView {
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+    Actions actions;
+
+    public BaseView(WebDriver driver) {
+        this.driver = driver;
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
+    }
+}
---
+++ WebUI_Java/Lesson2/example_andre/src/main/java/Lesson6/MainPage.java
@@ -0,0 +1,10 @@
+package Lesson6;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainPage extends BaseView{
+
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+}
